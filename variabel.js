61e14770fb7a50f9c213b799cf8ffe481089c53c
fikrialfian <91277901+Cryzepy@users.noreply.github.com>
2023-07-25 00:55:56 +0700

    Delete variabel.js

--- variabel.js
+++
@@ -1,7 +0,0 @@
-// const serverURL = "http://localhost:3001/admin/savxr6wecvrt46rt376rtb3y"
-
-const variabel = {
-	serverURL: "https://project-course-2.vercel.app/admin/savxr6wecvrt46rt376rtb3y",
-	key: "savxr6wecvrt46rt376rtb3y"
-}
-export default variabel
