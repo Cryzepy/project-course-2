5d070073e2207a76c5cf49c5045fe780541967d4
fikrialfian <91277901+Cryzepy@users.noreply.github.com>
2023-07-24 20:15:23 +0700

    Add files via upload

--- client/src/pages/course.jsx
+++ client/src/pages/course.jsx
@@ -1,121 +1,32 @@
-import $ from "jquery"
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-
 import "../css/course.css"
-import logo from "../images/Logo PKM.png"
+import logo from "../images/Logo PKM PM 1.jpg"
 
-import Footer from "../components/footer.jsx"
-import BtnLogout from "../components/btnlogout.jsx"
-import ErrorPage from "../pages/404.jsx"
-
-import tokenUtil from "../utils/api/token.js"
-import videoUtil from "../utils/api/video.js"
-import getTitle from "../utils/utility/getVideoTitle.js"
-
-const CoursePage = () => {
-
-   const navigate = useNavigate()
-
-    function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  }
-
-   const [username, setUsername] = useState("")
-   const [data, setData] = useState(false)
-   const [title,setTitle] = useState({
-    id: null,
-    element: null
-   })
-
-
-   
-   useEffect(() => {
-
-    const token = getCookie("token")
-
-    tokenUtil.getToken(token,{ navigate, setUsername })
-    videoUtil.getAll(setData,[runjQuery])
-
-     function runjQuery(){
-      $(".input-file").hide()
-
-         $(".btn-submit").click(function(e){
-            $(".btn-submit").show()
-            $(".input-file").hide()
-            $(e.target).prev().show()
-            $(this).hide()
-         })
-
-         $(".btn-close-addfile").click(function(e){
-            $(".btn-submit").show()
-            $(".input-file").hide()
-         })
- 
-     }
-
-   },[])
-
+const Course = () => {
 	return (
-      <>
-            <header>
-               <span className="brand">
-                  <img src={logo} alt="Logo PKM PM 1" />
-                  <span className="txt-brand">PKM-PM | Universitas Muhammadiyah Malang</span>
-               </span>
-               <BtnLogout username={username} />
-            </header>
-      
-            <div className="courses-container">
-               <h2 className="course-title">Courses</h2>
-               { !data.length && (<span>Tidak ada video</span>) }
-               { data.length > 0 && (
-                  <div className="courses">
-                  {
-                     data.map((id, index) => {
-                        const linkVideo = `https://www.youtube.com/watch?v=${id.url}`
-                        const linkThumb = `https://img.youtube.com/vi/${id.url}/maxresdefault.jpg`
-                        const idVideo = `video-title-${index}`
+		<>
+			 <header>
+         <span class="brand">
+            <img src={logo} alt="Logo PKM PM 1" />
+            <span class="txt-brand">PKM-PM | Universitas Muhammadiyah Malang</span>
+         </span>
+         <span id="btn-logout">Log Out (usernmae)</span>
+      </header>
 
-                        const element = $(`#video-title-${index}`)[0]
-
-                        getTitle(id.url,element)
-
-                        return (
-                           <div className="course" key={index}>
-                              <a href={linkVideo} target="_blank">
-                                 <img src={linkThumb} alt="thumbnail"/>
-                              </a>
-                              <h6 className="video-title" id={idVideo}>Judul Tidak Tersedia</h6>
-                              <div className="input-file">
-                              {
-                                id.linkTugas && (
-                                  <a href={id.linkTugas} className="button send-data" target="_blank">
-                                    <button className="btn btn-success" id="btn-send-tugas">kirim</button>
-                                  </a>
-                                )
-                              }
-
-                              {
-                                !id.linkTugas && ( <a className="btn btn-success button send-data" id="btn-send-tugas">Tidak Ada Tugas</a> )
-                              }
-
-                                 <button className="button btn btn-danger btn-close-addfile">tutup</button>
-                              </div>
-                              <button className="submit-tugas btn btn-primary btn-submit w-100">Submit Tugas</button>
-                           </div>
-                        )
-                     })
-                  }
+      <div class="courses-container">
+         <h2 class="course-title">Courses</h2>
+            <span>Tidak ada video</span>
+            <div class="courses">
+                  <div class="course">
+                     <a href="https://www.youtube.com/watch?v=<%=id.idVideo%>">
+                        <img src="https://img.youtube.com/vi/<%=id.idVideo%>/maxresdefault.jpg" alt="thumbnail" width="320" height="180" />
+                     </a>
                   </div>
-                  )
-               }
             </div>
-            <Footer />
-            </>
-            )
+      </div>
+
+      <footer>PKM-PM | Universitas Muhammadiyah Malang © 2023</footer>
+		</>
+	)
 }
 
-export default CoursePage
+export default Course
