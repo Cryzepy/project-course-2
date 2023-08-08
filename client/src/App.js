fe3c69869d60ee1e156188e8057cedfa580e92f0
fikrialfian <91277901+Cryzepy@users.noreply.github.com>
2023-08-09 06:10:55 +0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,24 @@ import Course from "./pages/course.jsx"
 import LoginPage from "./pages/login.jsx"
 import Admin from "./pages/admin.jsx"
 
+import AOS from "aos"
+import 'aos/dist/aos.css'
+
 function App () {
-  return (
-    <BrowserRouter>
-    	<Routes>
-    		<Route path="/" element={<LoginPage />} />
-    		<Route path="/course" element={<Course />} />
-    		<Route path="/admin" element={<Admin />} />
-    	</Routes>
-    </BrowserRouter>
-  )
+
+	AOS.init({
+		mirror: false
+	})
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/course" element={<Course />} />
+				<Route path="/admin" element={<Admin />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
